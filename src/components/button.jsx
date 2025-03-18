590daea6201779dafabb2b972b0a2a8f1a506979
mukesh vantakula <durgasaimukeshvantakula5764@gmail.com>
2025-03-18 09:55:47 +0530

    view product button added

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,19 @@
 // write button card here
+import React from 'react';
+
+const ViewProductButton = () => {
+  return (
+    <button style={{
+      padding: '10px 20px',
+      backgroundColor: '#007bff',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer'
+    }}>
+      View Product
+    </button>
+  );
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,21 @@
 // write product card here
+import React from 'react';
+import ViewProductButton from '../components/button';
+
+
+const ProductCard = () => {
+  const productImage = 'https://static.foxdealer.com/686/2021/03/2019_Dodge_Challenger_-_Exterior_Side_View.jpg';
+  const productName = 'dodge';
+  const productPrice = '₹80 lakhs';
+
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '16px', textAlign: 'center', borderRadius: '8px', width: '250px' }}>
+      <img src={productImage} alt="Product" style={{ width: '100%', height: '150px', objectFit: 'cover', borderRadius: '8px' }} />
+      <h3>{productName}</h3>
+      <p style={{ color: 'green', fontWeight: 'bold' }}>{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default ProductCard;
